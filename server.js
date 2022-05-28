e3aa658e0a5d51bbe3a586b8ef18a0d7cb522f7e
shintack <shintackeror404@gmail.com>
2022-05-28 11:19:17 +0700

    add docker compose api gateway

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`app listening at http://localhost:${port}`);
 });
