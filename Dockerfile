338a96a55b2692c112359af59f558dbb706693f8
shintack <shintackeror404@gmail.com>
2022-06-03 19:50:41 +0700

    update docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,7 @@
-FROM node:16
+FROM node:16-alpine
 
-# Create app directory
 WORKDIR /usr/src/app
 
-# Install app dependencies
-# A wildcard is used to ensure both package.json AND package-lock.json are copied
-# where available (npm@5+)
 COPY package*.json ./
 
 RUN npm install
@@ -16,4 +12,5 @@ RUN npm install
 COPY . .
 
 EXPOSE 4000
+
 CMD [ "node", "server.js" ]
--- logging.js
+++ logging.js
@@ -1,33 +1,36 @@
-// const morgan = require("morgan");
-const mongooseMorgan = require("mongoose-morgan");
+const morgan = require("morgan");
+// const mongooseMorgan = require("mongoose-morgan");
 const setupLogging = (app) => {
-  // app.use(morgan("combined"));
+  app.use(morgan("combined"));
   // app.use(morgan("tiny"));
   // Logger
-  app.use(
-    mongooseMorgan(
-      {
-        connectionString: "mongodb://localhost:27017/logs-db",
-      },
-      {},
-      function (tokens, req, res) {
-        return JSON.stringify({
-          method: tokens.method(req, res),
-          url: tokens.url(req, res),
-          status: tokens.status(req, res),
-          referrer: tokens.referrer(req, res),
-          // request: tokens.req(req, res), // TODO: MSH BUG
-          response: tokens.res(req, res, "content-length"),
-          responseTime: tokens["response-time"](req, res) + " ms",
-          totalTime: tokens["total-time"](req, res) + " ms",
-          remoteAddr: tokens["remote-addr"](req, res),
-          remoteUser: tokens["remote-user"](req, res),
-          httpVersion: tokens["http-version"](req, res),
-          userAgent: tokens["user-agent"](req, res),
-        });
-      }
-    )
-  );
+  // app.use(
+  //   mongooseMorgan(
+  //     {
+  // FIXME: ERROR CONNECT MMONGODB DOCKER
+  //       connectionString: "mongodb://gateway-db:27017/logs-db",
+  //       user: "root",
+  //       pass: "secret",
+  //     },
+  //     {},
+  //     function (tokens, req, res) {
+  //       return JSON.stringify({
+  //         method: tokens.method(req, res),
+  //         url: tokens.url(req, res),
+  //         status: tokens.status(req, res),
+  //         referrer: tokens.referrer(req, res),
+  //         // request: tokens.req(req, res), // TODO: MSH BUG
+  //         response: tokens.res(req, res, "content-length"),
+  //         responseTime: tokens["response-time"](req, res) + " ms",
+  //         totalTime: tokens["total-time"](req, res) + " ms",
+  //         remoteAddr: tokens["remote-addr"](req, res),
+  //         remoteUser: tokens["remote-user"](req, res),
+  //         httpVersion: tokens["http-version"](req, res),
+  //         userAgent: tokens["user-agent"](req, res),
+  //       });
+  //     }
+  //   )
+  // );
 };
 
 exports.setupLogging = setupLogging;
