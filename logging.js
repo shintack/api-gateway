ed2bb4a7c45ae3f20463860727ee402c8d64e51d
shintack <shintackeror404@gmail.com>
2022-06-04 10:06:40 +0700

    change logger from mongodb to file

--- logging.js
+++ logging.js
@@ -1,36 +1,23 @@
 const morgan = require("morgan");
-// const mongooseMorgan = require("mongoose-morgan");
+const path = require("path");
+const rfs = require("rotating-file-stream"); // version 2.x
+const json = require("morgan-json");
+
+// create a rotating write stream
+const accessLogStream = rfs.createStream("access.log", {
+  size: "10M", // rotate every 10 MegaBytes written
+  interval: "1d", // rotate daily
+  compress: "gzip", // compress rotated files
+  path: path.join(__dirname, "log"),
+});
+
+const format = json(
+  `:remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :response-time ms`
+);
+
+// setup the logger
 const setupLogging = (app) => {
-  app.use(morgan("combined"));
-  // app.use(morgan("tiny"));
-  // Logger
-  // app.use(
-  //   mongooseMorgan(
-  //     {
-  // FIXME: ERROR CONNECT MMONGODB DOCKER
-  //       connectionString: "mongodb://gateway-db:27017/logs-db",
-  //       user: "root",
-  //       pass: "secret",
-  //     },
-  //     {},
-  //     function (tokens, req, res) {
-  //       return JSON.stringify({
-  //         method: tokens.method(req, res),
-  //         url: tokens.url(req, res),
-  //         status: tokens.status(req, res),
-  //         referrer: tokens.referrer(req, res),
-  //         // request: tokens.req(req, res), // TODO: MSH BUG
-  //         response: tokens.res(req, res, "content-length"),
-  //         responseTime: tokens["response-time"](req, res) + " ms",
-  //         totalTime: tokens["total-time"](req, res) + " ms",
-  //         remoteAddr: tokens["remote-addr"](req, res),
-  //         remoteUser: tokens["remote-user"](req, res),
-  //         httpVersion: tokens["http-version"](req, res),
-  //         userAgent: tokens["user-agent"](req, res),
-  //       });
-  //     }
-  //   )
-  // );
+  app.use(morgan(format, { stream: accessLogStream }));
 };
 
 exports.setupLogging = setupLogging;
